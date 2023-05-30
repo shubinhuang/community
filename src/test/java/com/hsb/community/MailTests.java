156644a27144f36a58530aa8aea6989c9e78c0e4
shubinhuang <1539769513@qq.com>
2023-05-30 14:19:26 +0800

    send mail

---
+++ src/test/java/com/hsb/community/MailTests.java
@@ -0,0 +1,22 @@
+package com.hsb.community;
+
+import com.hsb.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+    @Autowired
+    private MailClient mailClient;
+
+    @Test
+    public void testTextMail() {
+        mailClient.sendMail("18859458156@163.com", "testText", "Hello World!");
+    }
+}
